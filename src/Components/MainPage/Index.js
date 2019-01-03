741eb4e7452b337b8adc6bff788584e82e7050f8
Eric Laursen <ericjlaursen@gmail.com>
2019-01-03 10:07:02 -0500

    Some default components

--- src/Components/MainPage/Index.js
+++ src/Components/MainPage/Index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-const App = () => {
-  return (
-    <div>
-      <p>Dummy React Component here!</p>
-    </div>
-  );
-};
-export default App;
-ReactDOM.render(<App />, document.getElementById("app"));
+import React from 'react';
+import Header from '../Header';
+import MessageList from '../MessageList';
+
+const MainPage = ({ currentUser, onLogout }) => (
+  <main>
+    <Header currentUser={currentUser} onLogout={onLogout} />
+    <MessageList currentUser={currentUser} />
+  </main>
+);
+
+export default MainPage;
