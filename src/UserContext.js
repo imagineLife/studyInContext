db2f33a65b2ba3fecf8d09c3626832b1fd192ad0
Eric Laursen <ericjlaursen@gmail.com>
2019-01-04 20:05:40 -0500

    begins a UserProvider class, rendering child props

--- src/UserContext.js
+++ src/UserContext.js
@@ -3,4 +3,15 @@ import React from 'react'
 //contains UserContext.Consumer && UserContext.Provider
 const UserContext = React.createContext()
 
+class UserProvider extends React.Component{
+	render(){
+		return(
+			<Provider>
+				{this.props.children}
+			</Provider>
+
+		)
+	}
+}
+
 export default UserContext;
