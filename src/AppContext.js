1eb4e0a5ae746a57be59be73e3c959cb9915adef
Eric Laursen <ericjlaursen@gmail.com>
2019-01-04 20:17:08 -0500

    Removes stateful data, sends to UserContext

--- src/AppContext.js
+++ src/AppContext.js
@@ -3,42 +3,26 @@ import ReactDOM from 'react-dom';
 import LoginPage from './ContextComponents/LoginPage';
 import MainPage from './ContextComponents/MainPage';
 import './main.css';
-import UserContext from './UserContext'
+import {UserProvider, UserConsumer} from './UserContext'
 
 export default class Root extends React.Component {
   constructor(props){
     super(props)
-    this.handleLogin = this.handleLogin.bind(this)
     this.state = {}
   }
-  
-  state = {
-    currentUser: null
-  };
-
-  handleLogin = user => {
-    this.setState({ currentUser: user });
-  };
-
-  handleLogout = () => {
-    this.setState({ currentUser: null });
-  };
 
   render() {
     return (
-      <UserContext.Provider
-        value={{
-          user: this.state.currentUser,
-          onLogin: this.handleLogin,
-          onLogout: this.handleLogout
-        }}
-      >
-        {this.state.currentUser ? (
-          <MainPage />
-        ) : (
-          <LoginPage />
-        )}
-      </UserContext.Provider>
+      <UserProvider>
+        <UserConsumer>
+          {({user}) =>
+          user ? (
+            <MainPage />
+          ) : (
+            <LoginPage />
+          )}
+        </UserConsumer>
+      </UserProvider>
     );
   }
 }
