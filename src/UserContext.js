01b9f457730fa91b237062e003e01e5923201aa0
Eric Laursen <ericjlaursen@gmail.com>
2019-01-04 20:17:54 -0500

    Uses state data and methods from App, updates context

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import { FAKE_USER } from './api'
 
 //contains UserContext.Consumer && UserContext.Provider
-const UserContext = React.createContext()
+const { Provider, Consumer } = React.createContext()
 
 class UserProvider extends React.Component{
+	state = {
+		currentUser: null
+	};
+
+	handleLogin = user => {
+		this.setState({ currentUser: user });
+	};
+
+	handleLogout = () => {
+		this.setState({ currentUser: null });
+	};
+
 	render(){
 		return(
-			<Provider>
+			<Provider
+				value={{
+		          user: this.state.currentUser,
+		          onLogin: this.handleLogin,
+		          onLogout: this.handleLogout
+		        }}>
 				{this.props.children}
 			</Provider>
 
@@ -14,4 +32,4 @@ class UserProvider extends React.Component{
 	}
 }
 
-export default UserContext;
+export {UserProvider, Consumer as UserConsumer};
