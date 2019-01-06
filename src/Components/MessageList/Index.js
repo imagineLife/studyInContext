a82fbe3b432cce853ad732c641ac11d08831a5c1
Eric Laursen <ericjlaursen@gmail.com>
2019-01-05 19:46:36 -0500

    Uses UserContext

--- src/Components/MessageList/Index.js
+++ src/Components/MessageList/Index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { UserConsumer } from '../../UserContext'
 
-const MessageList = ({ currentUser }) => (
-  <div className="MessageList">
-    <div className="no-messages">
-      Your mailbox is empty, {currentUser.firstName}! 🎉
-    </div>
-  </div>
+const MessageList = () => (
+  <UserContext>
+  	{ ({user}) => (
+		<div className="MessageList">
+			<div className="no-messages">
+				Your mailbox is empty, {user.firstName}! 🎉
+			</div>
+		</div>
+  	)}
+  </UserContext>
 );
 
 export default MessageList;
