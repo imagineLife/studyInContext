8ca22cca83014c8673beafa46764a5ac0db8940b
Eric Laursen <ericjlaursen@gmail.com>
2019-01-05 20:32:42 -0500

    sets class to camelCase

--- src/ContextComponents/Header/Index.js
+++ src/ContextComponents/Header/Index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import UserMenu from '../UserMenu';
+import './index.css'
 
 const Header = () => (
-  <header className="Header">
+  <header className="header">
     <h2>MyMail</h2>
     <UserMenu />
   </header>
