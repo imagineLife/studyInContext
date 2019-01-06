369f0e22fe6ba16a46b63f06e8ba7e950744fbf9
Eric Laursen <ericjlaursen@gmail.com>
2019-01-05 20:48:39 -0500

    Consumes EmailContext, renders list of emails

--- src/ContextComponents/MessageList/Index.js
+++ src/ContextComponents/MessageList/Index.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import {UserConsumer} from '../../UserContext';
+import { UserConsumer } from '../../UserContext';
+import { EmailConsumer } from '../../EmailContext';
 
 const MessageList = () => (
 	<UserConsumer> 
 		{({user}) => (
-			<div className="MessageList">
-			    <div className="no-messages">
-			      Your mailbox is empty, {user.firstName}! 🎉
-			    </div>
-			</div>
+			<EmailConsumer>
+				{({emails}) => (
+					<div className="MessageList">
+					    {emails.length === 0 ?
+					    	<div className="no-messages">
+						      Your mailbox is empty, {user.firstName}! 🎉
+						    </div>
+						    : <ul>
+						    	{emails.map(email => (
+						    		<Email key={email.id} 
+						    		onClick={() => onSelectEmail(email)}
+						    		email={email} />
+						    	))}
+						    </ul>
+					    }
+					    
+					</div>
+				)}
+			</EmailConsumer>
 		)}  
 	</UserConsumer>
 );
+
+
+const Email = ({email, onClick}) => (
+	<li onClick={onClick}>
+		<div className="emailSubject">{email.subject}</div>
+		<div className="emailPreview">{email.preview}</div>
+	</li>
+)
 
 export default MessageList;
