e5d455c0b4d728144c42916c8bf6428b905d7879
Eric Laursen <ericjlaursen@gmail.com>
2019-01-05 19:43:58 -0500

    Updates code layout

--- src/EmailContext.js
+++ src/EmailContext.js
@@ -1,57 +1,43 @@
 import React from 'react';
 import { fetchEmails } from './apiData';
 
-const { Provider, Consumer }  = React.createContext();
+const { Provider, Consumer } = React.createContext();
 
+class EmailProvider extends React.Component {
+  state = {
+    emails: [],
+    currentEmail: null,
+    error: null,
+    loading: false
+  };
 
-//holds state, to be distributed with provider
-class EmailProvider extends React.Component{
+  componentDidMount() {
+    this.setState({ loading: true, error: null });
+    fetchEmails()
+      .then(emails =>
+        this.setState({ loading: false, emails })
+      )
+      .catch(error =>
+        this.setState({ loading: false, error })
+      );
+  }
 
-	//some Context State
-	state = {
-		emails: [],
-		currentEmail: null,
-		error: null,
-		loading: false
-	}
+  handleSelectEmail = email => {
+    this.setState({ currentEmail: email });
+  };
 
-	//fetches emails from api when Context is run
-	componentDidMount(){
-		
-		this.setState({
-			loading: true,
-			error: null
-		})
-
-		fetchEmails().then(res => {
-			//put emails in state
-			this.setState({
-				loading: false,
-				emails: res
-			})
-		}).catch(err => {
-			this.setState({
-				loading: false,
-				error: err
-			})
-		})
-
-	}
-
-	handleEmailSelection = (selectedEmail) => {
-		this.setState({currentEmail: selectedEmail})
-	}
-
-	render(){
-		return(
-			<EmailProvider value={{
-				...this.state,
-				onSelectEmail: handleEmailSelection
-			}}>
-				{this.props.children}
-			</EmailProvider>
-		)
-	}
+  render() {
+    return (
+      <Provider
+        value={{
+          ...this.state,
+          onSelectEmail: this.handleSelectEmail
+        }}
+      >
+        {this.props.children}
+      </Provider>
+    );
+  }
 }
 
-export { EmailProvider, Consumer as EmailConsumer }
+export { EmailProvider, Consumer as EmailConsumer };
