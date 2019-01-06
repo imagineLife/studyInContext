f74ef42d07deb5ff7204106e876fef08aebd7f06
Eric Laursen <ericjlaursen@gmail.com>
2019-01-05 19:14:16 -0500

    Begins EmailContext development

---
+++ src/EmailContext.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { fetchEmails } from './apiData';
+
+const { Provider, Consumer }  = React.createContext();
+
+
+//holds state, to be distributed with provider
+class EmailProvider extends React.Component{
+
+	//some Context State
+	state = {
+		emails: [],
+		currentEmail: null,
+		error: null,
+		loading: false
+	}
+
+	//fetches emails from api when Context is run
+	componentDidMount(){
+		
+		this.setState({
+			loading: true,
+			error: null
+		})
+
+		fetchEmails().then(res => {
+			//put emails in state
+			this.setState({
+				loading: false,
+				emails: res
+			})
+		}).catch(err => {
+			this.setState({
+				loading: false,
+				error: err
+			})
+		})
+
+	}
+	render(){
+		return()
+	}
+}
