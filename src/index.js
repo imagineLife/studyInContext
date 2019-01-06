5c8865716b8209c075020a56a88ebf4c09884aff
Eric Laursen <ericjlaursen@gmail.com>
2019-01-05 19:15:35 -0500

    Runs appContext

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // import App from "./AppContext";
-// import AppContext from "./AppContext";
-import AppButtons from "./AppButtons";
+import AppContext from "./AppContext";
+// import AppButtons from "./AppButtons";
 import style from "./main.css";
