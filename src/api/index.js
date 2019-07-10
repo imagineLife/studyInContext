1368baf8e066d192dfc4e3a7fb7f9c2f5dd821b9
Eric Laursen <ericjlaursen@gmail.com>
2019-07-10 06:45:05 -0400

    updates fake un && pw

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 export const FAKE_USER = {
-  firstName: 'Jake',
-  lastName: 'Laursen',
-  username: 'Jake',
+  firstName: 'water',
+  lastName: 'melon',
+  username: 'water',
   avatar:
     'https://avatars2.githubusercontent.com/u/24195040?v=4'
 };
@@ -9,7 +9,7 @@ export const FAKE_USER = {
 export function login(username, password) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (username === 'Jake' && password === 'secret') {
+      if (username === 'water' && password === 'melon') {
         resolve(FAKE_USER);
       } else {
         reject({ message: 'Invalid username or password' });
