a35e9cd0fb57cc7671a3f85f16d22b45730d9423
Eric Laursen <ericjlaursen@gmail.com>
2019-07-10 12:18:22 -0400

    Removes logs

--- src/AppHooks.js
+++ src/AppHooks.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginPage from './Components/LoginPage/Hooks';
 import MainPage from './Components/MainPage/Hooks';
-import UserContext from './UserContextHooks'
+import {UserContext} from './UserContextHooks'
 import './main.css';
 
 export default function Root(){
   let [currentUser, setCurrentUser] = React.useState(null)
+  
   
   const handleLogin = user => {
     console.log('handleLogin USER => ')
@@ -17,10 +18,6 @@ export default function Root(){
   const handleLogout = () => {
     setCurrentUser(null);
   };
-
-  console.log('currentUser')
-  console.log(currentUser)
-  
     
   //if logged-in, app about current user
   return currentUser ? (
--- src/Components/MainPage/Hooks.js
+++ src/Components/MainPage/Hooks.js
@@ -3,11 +3,6 @@ import Header from '../Header/Hooks';
 import MessageList from '../MessageList/Hooks';
 
 const MainPage = ({ currentUser, onLogout }) => {
-	console.log('currentUser')
-	console.log(currentUser)
-	console.log('onLogout')
-	console.log(onLogout)
-	
 	return(
 	  <main className="myMain">
 	    <Header currentUser={currentUser} onLogout={onLogout} />
