dd017f17c0dfa7e17fbeff8133bf647f6c860872
Eric Laursen <ericjlaursen@gmail.com>
2019-07-10 06:45:57 -0400

    Includes hooks version of app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+// import App from "./App";
+import AppHooks from "./AppHooks";
 // import App from "./AppContext";
-import AppContext from "./AppContext";
+// import AppContext from "./AppContext";
 // import AppButtons from "./AppButtons";
 import style from "./main.css";
