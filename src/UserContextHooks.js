198ce845430ead1c897f823070953251bbbe8a1a
Eric Laursen <ericjlaursen@gmail.com>
2019-07-11 05:26:51 -0400

    Includes UserProvider const

--- src/UserContextHooks.js
+++ src/UserContextHooks.js
@@ -3,6 +3,12 @@ import React from 'react';
 const UserContext = React.createContext(); 
 const {Provider, Consumer} = UserContext
 
+const UserProvider = (props) => {
+	return(<Provider>
+		{props.children}
+	</Provider>)
+}
+
 // export default Provider;
 export {Provider as UserProvider, Consumer as UserConsumer, UserContext}
 //Contains Context.Consumer, Context.Provider
