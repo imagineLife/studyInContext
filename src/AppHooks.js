97200e5b9bdea7fdc5b7427c2ede6204cec7cd00
Eric Laursen <ericjlaursen@gmail.com>
2019-07-11 06:57:46 -0400

    Uses Context Provider && Consumer

--- src/AppHooks.js
+++ src/AppHooks.js
@@ -2,41 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginPage from './Components/LoginPage/Hooks';
 import MainPage from './Components/MainPage/Hooks';
-import {UserContext} from './UserContextHooks'
+import {UserContext, UserProvider, UserConsumer} from './UserContextHooks'
 import './main.css';
 
 export default function Root(){
   
-  let [currentUser, setCurrentUser] = React.useState(null)
-  
-  const handleLogin = user => {
-    console.log('handleLogin USER => ')
-    console.log(user)
-    setCurrentUser(user);
-  };
-
-  const handleLogout = () => {
-    setCurrentUser(null);
-  };
     
   //if logged-in, app about current user
-  return currentUser ? (
-    <UserContext.Provider value={{
-      firstName: currentUser.firstName,
-      lastName: currentUser.lastName,
-      avatar: currentUser.avatar,
-      handleLogout,
-      handleLogin
-    }}>
-      <MainPage/>
-    </UserContext.Provider>
-
-  //if not logged-in
-  ) : (
-    <UserContext.Provider value={handleLogin}>
-      <LoginPage />
-    </UserContext.Provider>  
-  );
+  return(
+    <UserProvider>
+      <UserConsumer>
+        { ( {currentUser} ) =>  
+           currentUser ? (<MainPage/>) : (<LoginPage />)
+        }
+      </UserConsumer>
+    </UserProvider>
+  )
 }
 
 ReactDOM.render(<Root />, document.getElementById('app'));
