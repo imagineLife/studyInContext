95ecc7398633334c0b4d1b1da5963bbf1501c5f9
Eric Laursen <ericjlaursen@gmail.com>
2019-07-10 21:33:48 -0400

    remves currentUser from props

--- src/AppHooks.js
+++ src/AppHooks.js
@@ -28,7 +28,6 @@ export default function Root(){
       avatar: currentUser.avatar
     }}>
       <MainPage
-        currentUser={currentUser}
         onLogout={handleLogout}
       />
     </UserContext.Provider>
