b347cf2795860c4af4ebd9a6baea1d135a0947d5
Eric Laursen <ericjlaursen@gmail.com>
2019-07-12 06:11:39 -0400

    mock emails && mock fetch fn

--- src/api/index.js
+++ src/api/index.js
@@ -6,6 +6,25 @@ export const FAKE_USER = {
     'https://avatars2.githubusercontent.com/u/24195040?v=4'
 };
 
+export const FAKE_EMAILS = [
+  {
+    id: 0,
+    subject: 'Hey Jake',
+    body: 'Yo, just wanted to say hey.'
+  },
+  {
+    id: 1,
+    subject: 'React is great',
+    body: 'I thought I should let you know.'
+  },
+  {
+    id: 2,
+    subject: 'REQUEST FOR ASSISTANCE',
+    body:
+      'If you send me your bank account number I will reward you with $10 million whole US dollars.'
+  }
+];
+
 export function login(username, password) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -14,6 +33,14 @@ export function login(username, password) {
       } else {
         reject({ message: 'Invalid username or password' });
       }
+    }, 300);
+  });
+}
+
+export function fetchEmails() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(FAKE_EMAILS);
     }, 300);
   });
 }
