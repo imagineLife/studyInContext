b1d37ecb37470408bcc2304dd8a679b617b6b81f
Eric Laursen <ericjlaursen@gmail.com>
2019-09-12 12:08:25 -0400

    Original Class-ed-Up Version

---
+++ src/App/Original.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginPage from '../Components/LoginPage';
+import MainPage from '../Components/MainPage';
+import '../main.css';
+
+export default class Root extends React.Component {
+  state = {
+    currentUser: null
+  };
+
+  handleLogin = user => {
+    this.setState({ currentUser: user });
+  };
+
+  handleLogout = () => {
+    this.setState({ currentUser: null });
+  };
+
+  render() {
+    console.log('made it here :) ')
+    
+    //if logged-in, state knows about current user
+    return this.state.currentUser ? (
+      <MainPage
+        currentUser={this.state.currentUser}
+        onLogout={this.handleLogout}
+      />
+
+    //if not logged-in
+    ) : (
+      <LoginPage onLogin={this.handleLogin} />
+    );
+  }
+}
+
+ReactDOM.render(<Root />, document.getElementById('app'));
